9d767f239e6baebb45bbe01533a539290a72db09
Christoph Burschka <christoph@burschka.de>
2013-08-25 17:34:00 +0200

    Find the word list when symlinked; make script executable.

--- byteword.py
+++ byteword.py
@@ -16,8 +16,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
+import os
 
-DICT='words.txt'
+DICT=os.path.dirname(os.path.realpath(__file__)) + '/words.txt'
 
 
 def reverse(words):
